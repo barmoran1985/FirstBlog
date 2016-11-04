e57b6bb76b26446919431f920b56be7f071472e1
Barry Moran <barmoran5@gmail.com>
2016-11-04 09:07:37 +0000

    first commit

---
+++ blog/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+from .models import Post
+
+# Create your views here.
+
+
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    return render(request, "blogposts.html", {'posts': posts})
+
+
+def post_details(request, id):
+    """
+    :param request:
+    :param id:
+    :return:
+
+    """
+    post = get_object_or_404(Post, pk=id)
+    post.views += 1
+    post.save()
+    return render(request, "postdetail.html", {'post': post})
+
